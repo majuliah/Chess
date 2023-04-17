742cbea3925ec5ed8b6ed6f29c01d3eb76cb8913
majulha <majulialcarvalho@gmail.com>
2023-04-17 16:02:56 -0300

    colocando pecas no tabuleiro

--- Program.cs
+++ Program.cs
@@ -11,6 +11,10 @@ namespace Chess
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
             Tela.ImprimirTabuleiro(tab);
+            
+            tab.ColocarPeca(new Torre(tab, Cor.Azul), new Posicao(0,0));
+            tab.ColocarPeca(new Torre(tab, Cor.Azul), new Posicao(1,3));
+            tab.ColocarPeca(new Torre(tab, Cor.Azul), new Posicao(2,4));
         }
     }
 }
